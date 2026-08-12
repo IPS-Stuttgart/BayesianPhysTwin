cdb0e59b922757550182fa2648e977eb8b5a8698
Florian Pfaff <6773539+FlorianPfaff@users.noreply.github.com>
2026-08-12 17:00:10 +0800

    Format propagated-state contract imports

--- tests/test_propagated_state_input_contract.py
+++ tests/test_propagated_state_input_contract.py
@@ -4,9 +4,7 @@ import numpy as np
 import pytest
 
 from bayesian_phystwin.propagated_state_belief import infer_propagated_state_belief
-from bayesian_phystwin.propagated_state_correction import (
-    select_propagated_state_update,
-)
+from bayesian_phystwin.propagated_state_correction import select_propagated_state_update
 
 
 _NON_BOOLEAN_AVAILABILITY = (
